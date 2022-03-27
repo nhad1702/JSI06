22f7fab2e2e495d021f3aaa4434b8fbad47db471
nhad1702 <anhducnguyenhuu2@gmail.com>
2022-03-27 10:29:20 +0700

    .

--- app.js
+++ app.js
@@ -5,18 +5,27 @@
 // let b = 2;
 // console.log(a + b);
 
-const getData = async (apiURL, callback) => {
-    try {
-        const request = fetch("https://https://reqres.in/api/users?page=2");
-        const data = await request.json();
-        console.log(data.data);
-    } catch (error) {
-        alert(error.massage)
-    }
-};
+// const getData = async (apiURL, callback) => {
+//     try {
+//         const request = fetch("https://https://reqres.in/api/users?page=2");
+//         const data = await request.json();
+//         console.log(data.data);
+//     } catch (error) {
+//         alert(error.massage)
+//     }
+// };
 
-const renderUsers = (users) => {
-    console.log("render user from API")
-}
+// const renderUsers = (users) => {
+//     console.log("render user from renderUsers")
+// }
 
-getData("https://https://reqres.in/api/users?page=2");
+// getData("https://https://reqres.in/api/users?page=2");
+
+export default function App() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
